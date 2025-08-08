b8e068a52b64d6380f496ed5bcd6b850bbe9d3b4
InWoong <dlsdnd122@naver.com>
2025-08-08 11:44:02 +0900

    feat: boj_1001 commit

---
+++ src/boj_1001/Main.java
@@ -0,0 +1,14 @@
+package boj_1001;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int n = num.nextInt();
+        int m = num.nextInt();
+
+        int result = n - m;
+        System.out.println(result);
+    }
+}
