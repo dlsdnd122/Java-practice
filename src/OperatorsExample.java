ee3414bb6a904d95ec1eb41fa21ae925ea4815dc
InWoong <dlsdnd122@naver.com>
2025-08-08 17:52:04 +0900

    feat: 3과목 연산자 수업내용 update commit

---
+++ src/OperatorsExample.java
@@ -0,0 +1,35 @@
+public class OperatorsExample {
+
+    public static void main(String[] args) {
+//        int a = 5;
+//        int b = 8;
+//        int c = a + b;
+//
+//        a = a + 1;
+//
+//        boolean a = true;
+//        boolean b = false;
+//        int a = 6;
+//        int b = 7;
+//        boolean c = a > b;
+        // = : 대입 연산자
+        // == : 비교 연산자
+
+// 3.2 연산의 방향과 우선순위
+// 복잡한 연산식에는 괄호()를 사용해서 우선순위를 정해준다.
+        // ++a -> a = a + 1
+// quiz
+// varw * var3 가 먼저 그다음이 var1 + (var2 * var3) 더해주고, = 대입연산자로 result로
+
+
+// 3.3 단항 연산자
+// 부호 연산자 -에서 양수로 바꿀려면
+//        int num2 = -5;
+//        int num3 = num2 * -1; //로 많이씀
+
+// 3.4 이항 연산자
+// 논리 연산자
+
+
+    }
+}
