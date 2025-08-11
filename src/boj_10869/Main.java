da71b7809c6e619e82bf2e31d3b082793950627b
InWoong <dlsdnd122@naver.com>
2025-08-11 11:46:58 +0900

    feat: 4과목 실습 백준 문제 풀이 commit

---
+++ src/boj_10869/Main.java
@@ -0,0 +1,23 @@
+package boj_10869;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int add = a + b;
+        System.out.println(add);
+        int sub = a - b;
+        System.out.println(sub);
+        int mul = a * b;
+        System.out.println(mul);
+        int div = a / b;
+        System.out.println(div);
+        int mod = a % b;
+        System.out.println(mod);
+
+    }
+}
---
+++ src/boj_10926/Main.java
@@ -0,0 +1,13 @@
+package boj_10926;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+
+        String str = name + "??!";
+        System.out.println(str);
+    }
+}
