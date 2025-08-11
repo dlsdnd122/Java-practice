e10a2b2996dfaeb45e4093882abf2a7eaf20d226
InWoong <dlsdnd122@naver.com>
2025-08-11 16:29:34 +0900

    docs: if문 공부 update commit

--- src/ifExample.java
+++ src/ifExample.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class ifExample {
     public static void main(String[] args) {
+        // 조건문 실습 코드
         Scanner sc = new Scanner(System.in);
         System.out.print("점수를 입력해 주세요 : ");
         int score = sc.nextInt();
@@ -15,5 +16,10 @@ public class ifExample {
         } else {
             System.out.println("등급은 D입니다.");
         }
+
+
+
+
+
     }
 }
