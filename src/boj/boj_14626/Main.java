c280ff77cb8e46a8405c01ebb7fd74c9a0d7758d
InWoong <dlsdnd122@naver.com>
2025-08-13 00:33:33 +0900

    refactor: 백준 문제풀이 update commit

--- src/boj/boj_14626/Main.java
+++ src/boj/boj_14626/Main.java
@@ -1,5 +1,6 @@
 package boj.boj_14626;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,29 +12,39 @@ public class Main {
         int[] arr = new int[chars.length];
         int sum = 0;
         int x = (int)chars[chars.length -1] - '0';
-        // 10으로 둔 이유는 모든 값들은 1의 자리 숫자이기 때문에 10으로 두면 차별성을 줄 수 있다.
-        int m = 10;
+        // 99으로 둔 이유는 모든 값들은 1의 자리 숫자이기 때문에 3을 곱해봐야 27이므로 차별성을 줄 수 있다.
+        int m = 99;
         // 곱한 숫자, *기호 찾는 로직
-        for (int i = 0; i < chars.length - 1; i++) {
+        for (int i = 0; i <= chars.length - 1; i++) {
+            // 첫번째 숫자는 따지고보면 0번째 인덱스 이기 때문에 인덱스가 하나씩 밀린다.
             if (i % 2 != 0) {
-                arr[i] = (int)chars[i] - '0';
-            } else if (i % 2 == 0) {
+                // 짝수번째 숫자 인덱스 = 실제 수에서 홀수
                 arr[i] = ((int)chars[i] - '0' ) * 3;
-            } else if (chars[i] == '*' && i % 2 != 0) {
-                arr[i] = m;
-            } else if (chars[i] == '*' && i % 2 == 0) {
-                arr[i] = m;
+            } if (arr[i] % 2 == 0) {
+                // 홀수 인덱스 숫자 인덱스 = 실제 수에서 짝수
+                arr[i] = (int)chars[i] - '0';
+            } if (chars[i] == '*') {
+                if (i % 2 != 0) {
+                    arr[i] = m * 3;
+                    System.out.println("* : " + arr[i]);
+                } else {
+                    arr[i] = m;
+                    System.out.println("* : " + arr[i]);
+                }
+            } if (arr[i] == arr[chars.length -1]) {
+                arr[i] = x; // 인덱스 마지막값 x 들어가게 하기
             }
         }
 
-        int answer = 0;
-        // -2인 이유는 마지막 인덱스는 체크 기호이기 때문에 더해주면 안된다.
-        for (int j = 0; j < chars.length - 2; j++) {
-            sum = sum + arr[j];
-            // arr[j]에 들어간 값이 10이고, j가 홀수 일때,
-            if (arr[j] == 10 && j % 2 != 0) {
-                arr[j] =
-            }
+        System.out.println("arr 배열 : " + Arrays.toString(arr));
+
+        for (int i = 0; i < chars.length; i++) {
+            sum += arr[i];
         }
+        sum = sum - 99; // 최종 sum 값
+        // 이제 여기서 (sum(106) + ?)/10 = 3
+
+
+
     }
 }
