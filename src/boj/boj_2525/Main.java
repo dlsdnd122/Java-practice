36eadee7e0472e9cae28f633372560d204ad1a66
InWoong <dlsdnd122@naver.com>
2025-08-27 17:52:41 +0900

    feat: 백준 문제풀이 commit

--- src/boj/boj_2525/Main.java
+++ src/boj/boj_2525/Main.java
@@ -11,14 +11,25 @@ public class Main {
         sc.close();
 
         // m + time 이 60분을 안넘을때
-        if (m + time < 60) {
-            System.out.println(n + " " + (m + time));
-        } else if (m + time >= 60) { // m + time 이 60분을 넘을 때
-            System.out.println(n + 1 + " " + ((m + time) - 60));
-            if (m + time >= 120) {
 
-            } if (n >= 24) {   // m + time 이 60을 넘었는데
-                System.out.println((n - 24) + " " + (m + time - 60));
+        // m + time 이 60을 넘으면 -> m + 1 해주기
+        // m + 1 이 23을 넘으면 1로 바꿔주기
+        // m + time 이 120을 넘으면 -> m + 2 해주기
+        // 120을 넘었을때로 계산해주면 안됨
+        // 반복문으로 넘겨주자
+
+
+        if (m + time < 60) {    // m + time 이 60을 안넘었을때
+            m = m + time;
+            System.out.println(n + " " + m);
+        } else if (m + time >= 60) { // m + time 이 60분을 넘을 때
+            n = n + ((m + time) / 60);
+            m = (m + time) % 60;
+            System.out.println(n + " " + m);
+            // 여기서 오류 발생함.
+            if (n >= 24) {          // n을 더한값이 24이상인 경우
+                n = n - 24;
+                System.out.println(n + " " + m);
             }
         }
     }
