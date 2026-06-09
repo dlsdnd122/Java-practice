e84b6d8cb9c6c6435cbf7a7e647a59d0fd40d493
InWoong <dlsdnd122@naver.com>
2026-06-09 20:38:48 +0900

    feat: 프로그래머스 문제풀이 commit

---
+++ src/programmers/Solution10.java
@@ -0,0 +1,30 @@
+package programmers;
+
+public class Solution10 {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+
+        char[] arr1 = my_string.toCharArray();
+        char[] arr2 = overwrite_string.toCharArray();
+
+        for (int i = 0; i < arr2.length; i++) {
+            if (arr1[s+i] != arr2[i]) {
+                arr1[s+i] = arr2[i];
+            }
+
+        }
+
+        answer = new String(arr1);
+
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        String my_string = "He11oWor1d";
+        String overwrite_string = "lloWorl";
+        int s = 2;
+
+        System.out.println(new Solution10().solution(my_string, overwrite_string, s));
+    }
+}
--- src/programmers/Solution8.java
+++ src/programmers/Solution8.java
@@ -1,4 +1,16 @@
 package programmers;
 
+import java.util.Scanner;
+
 public class Solution8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        char[] arr = a.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
 }
