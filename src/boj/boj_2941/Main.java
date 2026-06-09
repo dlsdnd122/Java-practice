6f9e2af34258ccd00a69b905accf79797caf386d
InWoong <dlsdnd122@naver.com>
2026-06-09 18:42:23 +0900

    feat: 프로그래머스 문제풀이 commit

--- src/boj/boj_2941/Main.java
+++ src/boj/boj_2941/Main.java
@@ -1,4 +1,17 @@
 package boj.boj_2941;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        // 일단 문자를 받고,
+        // 문자열을 나누는걸로 검사한다. .subString
+
+
+
+
+    }
 }
