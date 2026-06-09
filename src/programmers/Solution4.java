4dbc9a5f5036941047a633460196b7663341cbf3
InWoong <dlsdnd122@naver.com>
2026-06-09 18:55:35 +0900

    feat: 프로그래머스 문제풀이 commit

---
+++ src/programmers/Solution4.java
@@ -0,0 +1,26 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Solution4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        char[] arr = a.toCharArray();
+
+        // 소문자 = 대문자, 대문자 = 소문자
+        for (int i = 0; i < arr.length; i++) {
+            if(Character.isLowerCase(arr[i])){
+                arr[i] = Character.toUpperCase(arr[i]);
+            } else  {
+                arr[i] = Character.toLowerCase(arr[i]);
+            }
+        }
+
+        // 출력
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
---
+++ src/programmers/Solution5.java
@@ -0,0 +1,8 @@
+package programmers;
+
+public class Solution5 {
+    public static void main(String[] args) {
+
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+    }
+}
