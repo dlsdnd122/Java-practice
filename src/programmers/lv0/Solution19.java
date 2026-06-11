eb4c2f9e3dfca46d06775d910fe5a63443b28e37
InWoong <dlsdnd122@naver.com>
2026-06-11 20:42:05 +0900

    feat: 프로그래머스 문제풀이 commit

--- src/programmers/lv0/Solution19.java
+++ src/programmers/lv0/Solution19.java
@@ -4,8 +4,41 @@ public class Solution19 {
     public int solution(String ineq, String eq, int n, int m) {
         int answer = 0;
 
+        // ineq에는 < , > 기호가 올 수 있고
+        // eq dpsms =, ! 기호가 올 수 있다
+        // ineq, eq 안에 들어가있는 문자를 파악한다
 
+        if (ineq == ">" && eq == "=") {
+            if (n >= m) {
+                answer = 1;
+            } else {
+                answer = 0;
+            }
+        } else if (ineq == "<" && eq == "=") {
+            if (n <= m) {
+                answer = 1;
+            } else {
+                answer = 0;
+            }
+        } else if (ineq == ">" && eq == "!") {
+            if (n > m) {
+                answer = 1;
+            } else  {
+                answer = 0;
+            }
+        } else if (ineq == "<" && eq == "!") {
+            if (n < m) {
+                answer = 1;
+            } else   {
+                answer = 0;
+            }
+        }
 
         return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution19 s = new Solution19();
+        System.out.println(s.solution("<", "=", 20, 50));
     }
 }
